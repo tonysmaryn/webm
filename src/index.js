e252f43c6e36461c95d42c45debcd6ef4d535767
Anton Maryn <tonysmaryn@gmail.com>
2021-05-14 23:23:41 +0300

    feat: implement main part

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './index.scss';
+import image1 from './images/1.jpg';
+import image2 from './images/2.jpg';
+import image3 from './images/3.jpg';
+import image4 from './images/4.jpg';
+import image5 from './images/5.jpg';
+import image6 from './images/6.jpg';
